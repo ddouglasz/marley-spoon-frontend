b9884398e67a99578e2e402a67130ca952483e07
ddouglasz <douglasegiemeh@gmail.com>
2021-09-05 23:56:53 +0100

    render irecipes data to ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,43 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { getRecipes } from "./api/recipes";
 import Layout from "../components/Layout";
 
 const RecipeList: NextPage = () => {
+  const [recipes, setRecipes] = useState<any>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      try {
+        const recipesData = await getRecipes();
+
+        setRecipes(recipesData);
+      } catch (error) {
+        setError(error);
+      }
+    }
+
+    fetchRecipes();
+  }, []);
+
+  if (!recipes.length) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <div>Recipe Listing Page</div>
+      <div>
+        <ul>
+          {recipes.map((recipe: any,i) => (
+            <li key={i}>{recipe.fields.title}</li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   );
 };
