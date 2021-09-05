79474bb5d499f6aaadc577cf176720c6562f46f1
ddouglasz <douglasegiemeh@gmail.com>
2021-09-05 08:40:27 +0100

    - take out unused code - setup page layout

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return <header>Marley Spoon Simple and Healthy Recipes</header>;
+};
+
+export default Header;
