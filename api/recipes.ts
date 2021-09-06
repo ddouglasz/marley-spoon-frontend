e7be51edaba7a39e39b64c09f6d06fa9e9e0b2d1
ddouglasz <douglasegiemeh@gmail.com>
2021-09-06 01:28:44 +0100

    move api folder to root of project

---
+++ api/recipes.ts
@@ -0,0 +1,15 @@
+import { contentfulClient } from '../utils/contentfulApi'
+
+
+export const getRecipes = async () => {
+    try {
+        const recipesResponse = await contentfulClient.getEntries()
+
+        const recipes = await recipesResponse.items.filter((recipe: any) => recipe.fields.description)
+        return recipes
+    } catch (error) {
+        return error.data
+    }
+}
+
+
