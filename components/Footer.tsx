d36f08bad7e2a10edcff928f6fc0c653737570be
ddouglasz <douglasegiemeh@gmail.com>
2021-09-07 16:24:47 +0100

    modify footer text and padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,13 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <span className={styles.footer_text}>Marley Spoon</span>
       <a
         href="https://github.com/ddouglasz/marley-spoon-frontend"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Check out project at{" "}
+        Github{" "}
         <span className={styles.logo}>
           <Image
             src="/githublogo.png"
