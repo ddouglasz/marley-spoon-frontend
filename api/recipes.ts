1996785dba292b111793704870c456a015d1221e
ddouglasz <douglasegiemeh@gmail.com>
2021-09-07 05:50:05 +0100

    implement api method to fetch recipe details

--- api/recipes.ts
+++ api/recipes.ts
@@ -5,8 +5,18 @@ export const getRecipes = async () => {
     try {
         const recipesResponse = await contentfulClient.getEntries()
 
-        const recipes = await recipesResponse.items.filter((recipe: any) => recipe.fields.description)
+        const recipes = recipesResponse.items.filter((recipe: any) => recipe.fields.description)
         return recipes
+    } catch (error) {
+        return error.data
+    }
+}
+
+export const getRecipe = async (recipeId: string) => {
+    try {
+        const recipeResponse = await contentfulClient.getEntry(recipeId)
+
+        return recipeResponse
     } catch (error) {
         return error.data
     }
