c1a4546c0e0ee27621b639cc6919d6cb18909441
ddouglasz <douglasegiemeh@gmail.com>
2021-09-07 16:25:29 +0100

    Style header text

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
-  return <header>Marley Spoon Simple and Healthy Recipes</header>;
+  return (
+    <header className={styles.header}>
+      <h1>Marley Spoon</h1>
+      <h2> Simple and Healthy Recipes</h2>
+    </header>
+  );
 };
 
 export default Header;
