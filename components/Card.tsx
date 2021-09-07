5eeb6b3153d0562d96f88d05712122032dd96b94
ddouglasz <douglasegiemeh@gmail.com>
2021-09-07 16:23:53 +0100

    create card component

---
+++ components/Card.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../styles/Card.module.css";
+
+interface ICardProps {
+  children: React.ReactNode;
+}
+
+const Card = ({ children, }: ICardProps) => {
+  return <div className={styles.card}>{children}</div>;
+};
+
+export default Card;
